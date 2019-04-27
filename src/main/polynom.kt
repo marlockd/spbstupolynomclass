645d6084b2a815dc3e3fe6b6e44f77132023ece8
marlockd <denislogvinov42@gmail.com>
2019-04-27 17:48:18 +0300

    polynom class

---
+++ src/main/polynom.kt
@@ -0,0 +1,189 @@
+package kotlinTasks.polynom
+import java.lang.Math.pow
+
+fun otl(po: Polynom) {
+    po.elements.forEach {
+        println(it.coef)
+        println(it.pow)
+        println()
+    }
+}
+
+
+/**
+ * Поправки к общему стандарту
+ * Класс теперь будет обращаться к членам со степенью 0 как к реальой степени только при конвертации со строки в полином
+ * Далее же степень 0 во всем коде означает ничто иное как отсутствие икса
+ * Например: Как отличить члены "x" и "1" между собой? Ведь убрав из свойства наличие икса,
+ * мы получим такое равенство, (coef = 1 pow = 1) которое одновременно полностью применимо и члену "1" и члену "x"
+ * Поэтому, было решено сделать число ноль в степени показателя того, что в члене отсутствует неизвестный X,
+ * за счет чего эта проблема становится не актуальной, а те участки кода, в которых теоритически возможно получение
+ * нуля в степени во время тех или иных действий, идёт автоматическая замена этого члена на единицу
+ */
+
+
+
+fun convert(poly: String): Polynom {
+    if (!Regex("""([-+]?((\d+x\^\d+)|(\d+x)|(x\^\d+)|\d+|x))+""").matches(poly))
+        throw IllegalArgumentException()
+    val tempList = mutableListOf<PolyPart>()
+    val list = Regex("""-""").replace(Regex("""\+""").
+            replace(poly, " "), " -").split(" ")
+    list.forEach {
+        if (it.matches(Regex("""-?\d+x\^\d+"""))) {
+            val parts = it.split("x^").map { it.toDouble() }
+            if (parts.last() == 0.0) tempList.add(PolyPart(1.0, 0))
+            else tempList.add(PolyPart(parts.first(), parts.last().toInt()))
+        }
+        if (it.matches(Regex("""-?x\^\d+"""))) {
+            val parts = it.split("x^")
+            if (parts.last().toInt() == 0) tempList.add(PolyPart(1.0, 0))
+            else if (parts.first() == "") tempList.add(PolyPart(1.0, parts.last().toInt()))
+            else if (parts.first() == "-") tempList.add(PolyPart(-1.0, parts.last().toInt()))
+        }
+        if (it.matches(Regex("""-?\d+\^\d+"""))) {
+            val parts = it.split("^")
+            if (parts.last().toInt() == 0) tempList.add(PolyPart(1.0, 0))
+            else tempList.add(PolyPart(pow(parts.first().toDouble(), parts.last().toDouble()), parts.last().toInt()))
+        }
+        if (it.matches(Regex("""-?\d+x"""))) tempList.add(PolyPart(it.replace("x", "").toDouble(), 1))
+        if (it.matches(Regex("""-?\d+"""))) tempList.add(PolyPart(it.toDouble(), 0))
+        if (it == "x") tempList.add(PolyPart(1.0, 1))
+        if (it == "-x") tempList.add(PolyPart(-1.0, 1))
+    }
+    return Polynom(tempList).shorten()
+}
+
+/**
+// отладка полинома
+fun out(po: Polynom) {
+    val res = mutableListOf<String>()
+    fun sOut(pp: PolyPart): String {
+        if (pp.isX && pp.pow > 1) return pp.coef.toString() + "x^" + pp.pow.toString()
+        if (!pp.isX) return pow(pp.coef, pp.pow.toDouble()).toString()
+        if (pp.isX && pp.pow == 1) return pp.coef.toString() + "x"
+        return ""
+    }
+    res.add(sOut(po.elements.first()))
+    if (po.elements.size > 1)
+        for (i in 1 until po.elements.size) {
+            if (po.elements[i].coef > 0) res.add("+")
+            res.add(sOut(po.elements[i]))
+        }
+    println(res.joinToString(""))
+}
+*/
+
+data class Polynom(val elements: MutableList<PolyPart>) {
+
+    // сравнение полиномов между собой
+    fun isEqual(other: Polynom): Boolean = elements == other.elements
+
+    // замена знаков в полиноме
+    fun changeSign(): Polynom = Polynom(elements.map { PolyPart(it.coef * -1, it.pow) }.toMutableList())
+
+    // вычисление при определенном целом значении X
+    fun calcX(x: Int): Double {
+        val sumList = mutableListOf<Double>()
+        elements.forEach {
+            if (it.pow != 0) sumList.add(it.coef * pow(x.toDouble(), it.pow.toDouble()))
+            else sumList.add(it.coef * x)
+        }
+        return sumList.sum()
+    }
+
+    // упрощение полинома
+    fun shorten(): Polynom {
+        val res = mutableListOf<PolyPart>()
+        val subParts = mutableMapOf<Int, MutableList<Double>>()
+        elements.forEach {
+            if (subParts[it.pow].isNullOrEmpty()) subParts[it.pow] = mutableListOf(it.coef)
+            else subParts[it.pow]!!.add(it.coef)
+        }
+        subParts.forEach { res.add(PolyPart(it.value.sum(), it.key)) }
+        return Polynom(res.filter { it.coef != 0.0 }.sortedBy { it.pow }.toMutableList())
+    }
+
+    // сложение полиномов
+    fun addition(other: Polynom): Polynom {
+        val res = mutableListOf<PolyPart>()
+        val subList = mutableMapOf<Int, MutableList<Double>>()
+        elements.forEach {
+            if (subList[it.pow].isNullOrEmpty()) subList[it.pow] = mutableListOf(it.coef)
+            else subList[it.pow]!!.add(it.coef)
+        }
+        other.elements.forEach {
+            if (subList[it.pow].isNullOrEmpty()) subList[it.pow] = mutableListOf(it.coef)
+            else subList[it.pow]!!.add(it.coef)
+        }
+        subList.forEach { res.add(PolyPart(it.value.sum(), it.key)) }
+        return Polynom(res).shorten()
+    }
+
+    // вычитание полиномов
+    fun subtract(other: Polynom): Polynom = addition(other.changeSign()).shorten()
+
+    // умножение
+    fun multiply(other: Polynom): Polynom {
+        val subParts = mutableListOf<Polynom>()
+        for (i in 0 until elements.size) {
+            subParts.add(Polynom(mutableListOf()))
+            other.elements.forEach { its -> subParts[i].elements.add(elements[i].multiply(its)) }
+        }
+        var res = subParts.first()
+        for (j in 1 until subParts.size) { res = res.addition(subParts[j]) }
+        return res.shorten()
+    }
+
+    // деление
+    fun div(other: Polynom): Polynom = Polynom(elements).commonDivide(other).first
+
+    // остаток от деления
+    fun mod(other: Polynom): Polynom = Polynom(elements).commonDivide(other).second
+
+    // общее деление
+    /**
+     * Суть деления такова: есть делимое(dividend) есть делитель (divider)
+     * quotient - частное, tempPoly - временный полином умножения деления старших степенных членов на делитель
+     * а частное - оставшееся делимое после всех итераций
+     *
+     * Как считается кол-во итераций?
+     * Заметил, что количество итераций высчитываетя по разности степеней старших членов делимого и делителя + 1
+     */
+    fun commonDivide(divider: Polynom): Pair<Polynom, Polynom> {
+        var dividend = Polynom(elements)
+        val quotient = mutableListOf<PolyPart>()
+        val times = dividend.elements.maxBy { it.pow }!!.pow - divider.elements.maxBy { it.pow }!!.pow + 1
+        repeat(times) {
+            val tempPoly = mutableListOf<PolyPart>()
+            val higherDividend = dividend.elements.maxBy { it.pow }!!
+            val higherDivider = divider.elements.maxBy { it.pow }!!
+            quotient.add(higherDividend.divide(higherDivider))
+            for (i in 0 until divider.elements.size) tempPoly.add(divider.elements[i].multiply(higherDividend.divide(higherDivider)))
+            dividend = dividend.subtract(Polynom(tempPoly))
+        }
+        return Pair(Polynom(quotient).shorten(), dividend.shorten())
+    }
+
+
+}
+
+data class PolyPart(var coef: Double, val pow: Int) {
+
+    fun multiply(otherPart: PolyPart): PolyPart {
+        if ((pow != 0) && (otherPart.pow != 0)) return PolyPart(coef * otherPart.coef, pow + otherPart.pow)
+        if ((pow != 0) && (otherPart.pow == 0)) return PolyPart(coef * otherPart.coef, pow)
+        if ((pow == 0) && (otherPart.pow != 0)) return PolyPart(coef * otherPart.coef, otherPart.pow)
+        if ((pow == 0) && (otherPart.pow == 0)) return PolyPart(coef * otherPart.coef,0)
+        throw IllegalArgumentException()
+    }
+
+    fun divide(otherPart: PolyPart): PolyPart {
+        if (pow != 0 && otherPart.pow != 0 && (pow >= otherPart.pow))
+            return PolyPart(coef / otherPart.coef,pow - otherPart.pow)
+        throw IllegalArgumentException()
+    }
+
+
+}
+
---
+++ src/test/PolynomTest.kt
@@ -0,0 +1,107 @@
+package kotlinTasks.polynom
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class PolynomTest {
+
+    val poly1 = convert("7x^4+3x^3-6x^2+x-8")
+    val poly2 = convert("172x^4+34x-6x^2+x^4-x-4-8+2")
+    val poly3 = convert("2x^3+x^4-2x^2-x")
+    val poly4 = convert("4x^6+2x^7-4x^6-2x^4")
+    val poly5 = convert("10x^5+3x^4-12x^3+25x^2-2x+5")
+    val poly6 = convert("5x^2-x+2")
+    val poly7 = convert("2x^7+x^8-2x^6-x^5")
+    val poly8 = convert("x^8+4x^7+0x^6-9x^5+2x^4+2x^3")
+
+    @Test
+    fun convert() {
+        assertEquals(Polynom(mutableListOf(
+                PolyPart(7.0, 4),
+                PolyPart(3.0, 3),
+                PolyPart(-6.0, 2),
+                PolyPart(1.0, 1),
+                PolyPart(-8.0, 0)
+        )).shorten(), convert("7x^4+3x^3-6x^2+x-8"))
+        assertEquals(Polynom(mutableListOf(
+                PolyPart(5.0, 2),
+                PolyPart(-1.0, 1),
+                PolyPart(2.0, 0)
+        )).shorten(), convert("5x^2-x+2"))
+        assertEquals(Polynom(mutableListOf(
+                PolyPart(2.0, 7),
+                PolyPart(1.0, 8),
+                PolyPart(-2.0, 6),
+                PolyPart(-1.0, 5)
+        )).shorten(), convert("2x^7+x^8-2x^6-x^5"))
+        assertEquals(Polynom(mutableListOf(PolyPart(2.0, 0))), convert("2"))
+    }
+
+
+    @Test
+    fun isEqual() {
+        assertTrue(poly1.isEqual(poly1))
+        assertTrue(poly3.isEqual(poly3))
+        assertFalse(poly1.isEqual(poly2))
+        assertFalse(poly2.isEqual(poly6))
+    }
+
+    @Test
+    fun changeSign() {
+        assertEquals(convert("-2x^7-x^8+2x^6+x^5"), poly7.changeSign())
+        assertEquals(convert("-7x^4-3x^3+6x^2-x+8"), poly1.changeSign())
+        assertEquals(convert("-172x^4-34x+6x^2-x^4+x+4+8-2"), poly2.changeSign())
+        assertEquals(convert("-2x^3-x^4+2x^2+x"), poly3.changeSign())
+    }
+
+    @Test
+    fun calcX() {
+        assertEquals(98.0, poly1.calcX(2), 1e-5)
+        assertEquals(14028.0, poly2.calcX(3), 1e-5)
+        assertEquals(2982.0, poly3.calcX(7), 1e-5)
+        assertEquals(224.0, poly4.calcX(2), 1e-5)
+        assertEquals(0.0, poly5.calcX(0), 1e-5)
+    }
+
+    @Test
+    fun addition() {
+        assertEquals(convert("14x^4+6x^3-12x^2+2x-16"), poly1.addition(poly1))
+        assertEquals(convert("176x^4+31x+19x^2-5+10x^5-12x^3"), poly2.addition(poly5))
+        assertEquals(convert("6x^7+1x^8-9x^5+2x^3"), poly4.addition(poly8))
+        assertEquals(convert("171x^4+33x-6x^2-10+2x^7"), poly2.addition(poly4))
+    }
+
+    @Test
+    fun subtract() {
+        assertEquals(convert("-166x^4+3x^3-32x+2"), poly1.subtract(poly2))
+        assertEquals(convert("2x^3+3x^4-2x^2-1x-2x^7"), poly3.subtract(poly4))
+        assertEquals(convert("10x^5+3x^4-12x^3+20x^2-1x+3"), poly5.subtract(poly6))
+        assertEquals(convert("-2x^7-2x^6+8x^5-2x^4-2x^3"), poly7.subtract(poly8))
+    }
+
+    @Test
+    fun multiply() {
+        assertEquals(convert("1211x^8+386x^5-1080x^6-1319x^4+519x^7-234x^3+141x^2-274x+80"), poly1.multiply(poly2))
+        assertEquals(convert("4x^10-4x^7+2x^11-4x^8-4x^9+4x^6+2x^5"), poly3.multiply(poly4))
+        assertEquals(convert("50x^7+5x^6-43x^5+143x^4-59x^3+77x^2-9x+10"), poly5.multiply(poly6))
+        assertEquals(convert("x^16+6x^15+6x^14-18x^13-20x^12+24x^11+9x^10-6x^9-2x^8"), poly7.multiply(poly8))
+    }
+
+    @Test
+    fun div() {
+        assertEquals(convert("2x^3+1x^2-3x+4"), poly5.div(poly6))
+        assertEquals(convert("4x^2-20x+102"), convert("4x^3+2x-11").div(convert("x+5")))
+        assertEquals(Polynom(mutableListOf()),
+                convert("7x^3+9x^2-5x+9").div(convert("5x^7+10x^6-17x^2+14x-7")))
+    }
+
+    @Test
+    fun mod() {
+        assertEquals(convert("8x-3"), poly5.mod(poly6))
+        assertEquals(convert("-521"), convert("4x^3+2x-11").mod(convert("x+5")))
+        assertEquals(convert("7x^3+9x^2-5x+9"),
+                convert("7x^3+9x^2-5x+9").mod(convert("5x^7+10x^6-17x^2+14x-7")))
+    }
+
+}
